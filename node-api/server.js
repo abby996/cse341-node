04ec9232e87a9cfa116d87b448d55c178e6a4be5
abby996 <abdiasbrevil@gmail.com>
2025-11-20 18:06:14 -0500

    node project

--- node-api/server.js
+++ node-api/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./data/database'); // ⬅️ GADE - data/database
+const connectDB = require('./config/database'); 
+
 
 // Load env vars
 dotenv.config();
